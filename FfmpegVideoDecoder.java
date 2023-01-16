6dcf5f42350a160f66d42db52ac35a96c202aed0
AhmedHumk <77048689+AhmedHumk@users.noreply.github.com>
2023-01-16 12:51:15 +0200

    Initial commit

---
+++ FfmpegVideoDecoder.java
@@ -0,0 +1,179 @@
+package com.google.android.exoplayer2.ext.ffmpeg;
+
+import android.view.Surface;
+import androidx.annotation.Nullable;
+import com.google.android.exoplayer2.Format;
+import com.google.android.exoplayer2.decoder.SimpleDecoder;
+import com.google.android.exoplayer2.decoder.VideoDecoderInputBuffer;
+import com.google.android.exoplayer2.decoder.VideoDecoderOutputBuffer;
+import com.google.android.exoplayer2.util.Assertions;
+import com.google.android.exoplayer2.util.Util;
+import java.nio.ByteBuffer;
+import java.util.List;
+import lib.kalu.exoplayer2.util.ExoLogUtil;
+
+/* loaded from: lib_mediaplayer_exo_r2.18.2_ff4.4_video_release_20230116.aar:classes.jar:com/google/android/exoplayer2/ext/ffmpeg/FfmpegVideoDecoder.class */
+final class FfmpegVideoDecoder extends SimpleDecoder<VideoDecoderInputBuffer, VideoDecoderOutputBuffer, FfmpegDecoderException> {
+    private static final int VIDEO_DECODER_SUCCESS = 0;
+    private static final int VIDEO_DECODER_ERROR_INVALID_DATA = -1;
+    private static final int VIDEO_DECODER_ERROR_OTHER = -2;
+    private static final int VIDEO_DECODER_ERROR_READ_FRAME = -3;
+    private static final int VIDEO_DECODER_ERROR_SEND_PACKET = -4;
+    private final String codecName;
+    private long nativeContext;
+    @Nullable
+    private final byte[] extraData;
+    private volatile int outputMode;
+
+    private native long ffmpegInitialize(String str, @Nullable byte[] bArr, int i);
+
+    private native long ffmpegReset(long j);
+
+    private native void ffmpegRelease(long j);
+
+    private native int ffmpegRenderFrame(long j, Surface surface, VideoDecoderOutputBuffer videoDecoderOutputBuffer, int i, int i2);
+
+    private native int ffmpegSendPacket(long j, ByteBuffer byteBuffer, int i, long j2);
+
+    private native int ffmpegReceiveFrame(long j, int i, VideoDecoderOutputBuffer videoDecoderOutputBuffer, boolean z);
+
+    public FfmpegVideoDecoder(int numInputBuffers, int numOutputBuffers, int initialInputBufferSize, int threads, Format format) throws FfmpegDecoderException {
+        super(new VideoDecoderInputBuffer[numInputBuffers], new VideoDecoderOutputBuffer[numOutputBuffers]);
+        if (!FfmpegLibrary.isAvailable()) {
+            throw new FfmpegDecoderException("Failed to load decoder native library.");
+        }
+        this.codecName = (String) Assertions.checkNotNull(FfmpegLibrary.getCodecName(format));
+        this.extraData = getExtraData(format.sampleMimeType, format.initializationData);
+        this.nativeContext = ffmpegInitialize(this.codecName, this.extraData, threads);
+        if (this.nativeContext == 0) {
+            throw new FfmpegDecoderException("Failed to initialize decoder.");
+        }
+        setInitialInputBufferSize(initialInputBufferSize);
+    }
+
+    @Nullable
+    private static byte[] getExtraData(String mimeType, List<byte[]> list) {
+        int num;
+        try {
+            if (null == list) {
+                throw new Exception();
+            }
+            try {
+                num = list.size();
+            } catch (Exception e) {
+                num = 0;
+            }
+            ExoLogUtil.log("FfmpegVideoDecoder => getExtraData => mimeType = " + mimeType + ", num = " + num);
+            if (num <= 0) {
+                throw new Exception();
+            }
+            int length = 0;
+            for (int i = 0; i < num; i++) {
+                length += list.get(i).length;
+            }
+            byte[] extra = new byte[length];
+            int destPos = 0;
+            for (int i2 = 0; i2 < num; i2++) {
+                byte[] bytes = list.get(i2);
+                System.arraycopy(bytes, 0, extra, destPos, bytes.length);
+                destPos = bytes.length;
+            }
+            return extra;
+        } catch (Exception e2) {
+            return null;
+        }
+    }
+
+    @Override // com.google.android.exoplayer2.decoder.Decoder
+    public String getName() {
+        return "ffmpeg" + FfmpegLibrary.getVersion() + "-" + this.codecName;
+    }
+
+    public void setOutputMode(int outputMode) {
+        this.outputMode = outputMode;
+    }
+
+    /* JADX INFO: Access modifiers changed from: protected */
+    /* JADX WARN: Can't rename method to resolve collision */
+    @Override // com.google.android.exoplayer2.decoder.SimpleDecoder
+    public VideoDecoderInputBuffer createInputBuffer() {
+        return new VideoDecoderInputBuffer(2);
+    }
+
+    /* JADX INFO: Access modifiers changed from: protected */
+    /* JADX WARN: Can't rename method to resolve collision */
+    @Override // com.google.android.exoplayer2.decoder.SimpleDecoder
+    public VideoDecoderOutputBuffer createOutputBuffer() {
+        return new VideoDecoderOutputBuffer((v1) -> {
+            releaseOutputBuffer(v1);
+        });
+    }
+
+    /* JADX INFO: Access modifiers changed from: protected */
+    @Override // com.google.android.exoplayer2.decoder.SimpleDecoder
+    @Nullable
+    public FfmpegDecoderException decode(VideoDecoderInputBuffer inputBuffer, VideoDecoderOutputBuffer outputBuffer, boolean reset) {
+        if (reset) {
+            this.nativeContext = ffmpegReset(this.nativeContext);
+            if (this.nativeContext == 0) {
+                return new FfmpegDecoderException("Error resetting (see logcat).");
+            }
+        }
+        ByteBuffer inputData = (ByteBuffer) Util.castNonNull(inputBuffer.data);
+        int inputSize = inputData.limit();
+        boolean needSendAgain = false;
+        int sendPacketResult = ffmpegSendPacket(this.nativeContext, inputData, inputSize, inputBuffer.timeUs);
+        if (sendPacketResult == -1) {
+            outputBuffer.setFlags(Integer.MIN_VALUE);
+            return null;
+        }
+        if (sendPacketResult == -3) {
+            needSendAgain = true;
+        } else if (sendPacketResult == -2) {
+            return new FfmpegDecoderException("ffmpegDecode error: (see logcat)");
+        }
+        boolean decodeOnly = inputBuffer.isDecodeOnly();
+        int getFrameResult = ffmpegReceiveFrame(this.nativeContext, this.outputMode, outputBuffer, decodeOnly);
+        if (getFrameResult == -4) {
+            return null;
+        }
+        if (getFrameResult == -2) {
+            return new FfmpegDecoderException("ffmpegDecode error: (see logcat)");
+        }
+        if (getFrameResult == -1) {
+            outputBuffer.addFlag(Integer.MIN_VALUE);
+        }
+        if (!decodeOnly) {
+            outputBuffer.format = inputBuffer.format;
+        }
+        ExoLogUtil.log("decodeOnly: " + decodeOnly);
+        if (needSendAgain) {
+            ExoLogUtil.log("timeUs=" + inputBuffer.timeUs + ", nendSendAagin");
+            return null;
+        }
+        return null;
+    }
+
+    /* JADX INFO: Access modifiers changed from: protected */
+    /* JADX WARN: Can't rename method to resolve collision */
+    @Override // com.google.android.exoplayer2.decoder.SimpleDecoder
+    public FfmpegDecoderException createUnexpectedDecodeException(Throwable error) {
+        return new FfmpegDecoderException("Unexpected decode error", error);
+    }
+
+    @Override // com.google.android.exoplayer2.decoder.SimpleDecoder, com.google.android.exoplayer2.decoder.Decoder
+    public void release() {
+        super.release();
+        ffmpegRelease(this.nativeContext);
+        this.nativeContext = 0L;
+    }
+
+    public void renderToSurface(VideoDecoderOutputBuffer outputBuffer, Surface surface) throws FfmpegDecoderException {
+        if (outputBuffer.mode != 1) {
+            throw new FfmpegDecoderException("Invalid output mode.");
+        }
+        if (ffmpegRenderFrame(this.nativeContext, surface, outputBuffer, outputBuffer.width, outputBuffer.height) == -2) {
+            throw new FfmpegDecoderException("Buffer render error: ");
+        }
+    }
+}
---
+++ FfmpegVideoRenderer.java
@@ -0,0 +1,106 @@
+package com.google.android.exoplayer2.ext.ffmpeg;
+
+import android.annotation.SuppressLint;
+import android.os.Handler;
+import android.util.Log;
+import android.view.Surface;
+import androidx.annotation.Nullable;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.Format;
+import com.google.android.exoplayer2.RendererCapabilities;
+import com.google.android.exoplayer2.decoder.CryptoConfig;
+import com.google.android.exoplayer2.decoder.Decoder;
+import com.google.android.exoplayer2.decoder.DecoderInputBuffer;
+import com.google.android.exoplayer2.decoder.DecoderReuseEvaluation;
+import com.google.android.exoplayer2.decoder.VideoDecoderOutputBuffer;
+import com.google.android.exoplayer2.util.Assertions;
+import com.google.android.exoplayer2.util.MimeTypes;
+import com.google.android.exoplayer2.util.TraceUtil;
+import com.google.android.exoplayer2.util.Util;
+import com.google.android.exoplayer2.video.DecoderVideoRenderer;
+import com.google.android.exoplayer2.video.VideoRendererEventListener;
+
+/* loaded from: lib_mediaplayer_exo_r2.18.2_ff4.4_video_release_20230116.aar:classes.jar:com/google/android/exoplayer2/ext/ffmpeg/FfmpegVideoRenderer.class */
+public class FfmpegVideoRenderer extends DecoderVideoRenderer {
+    private static final String TAG = "FfmpegVideoRenderer";
+    private static final int DEFAULT_NUM_OF_INPUT_BUFFERS = 4;
+    private static final int DEFAULT_NUM_OF_OUTPUT_BUFFERS = 4;
+    private static final int DEFAULT_INPUT_BUFFER_SIZE = ((Util.ceilDivide(1280, 64) * Util.ceilDivide(720, 64)) * 6144) / 2;
+    private final int numInputBuffers;
+    private final int numOutputBuffers;
+    private final int threads;
+    @Nullable
+    private FfmpegVideoDecoder decoder;
+
+    public FfmpegVideoRenderer(long allowedJoiningTimeMs, @Nullable Handler eventHandler, @Nullable VideoRendererEventListener eventListener, int maxDroppedFramesToNotify) {
+        this(allowedJoiningTimeMs, eventHandler, eventListener, maxDroppedFramesToNotify, Runtime.getRuntime().availableProcessors(), 4, 4);
+    }
+
+    public FfmpegVideoRenderer(long allowedJoiningTimeMs, @Nullable Handler eventHandler, @Nullable VideoRendererEventListener eventListener, int maxDroppedFramesToNotify, int threads, int numInputBuffers, int numOutputBuffers) {
+        super(allowedJoiningTimeMs, eventHandler, eventListener, maxDroppedFramesToNotify);
+        this.threads = threads;
+        this.numInputBuffers = numInputBuffers;
+        this.numOutputBuffers = numOutputBuffers;
+    }
+
+    @Override // com.google.android.exoplayer2.RendererCapabilities
+    @SuppressLint({"WrongConstant"})
+    public final int supportsFormat(Format format) {
+        String mimeType = (String) Assertions.checkNotNull(format.sampleMimeType);
+        if (!FfmpegLibrary.isAvailable() || !MimeTypes.isVideo(mimeType)) {
+            return 0;
+        }
+        if (!FfmpegLibrary.supportsFormat(format)) {
+            return RendererCapabilities.create(1);
+        }
+        if (format.drmInitData != null) {
+            return RendererCapabilities.create(2);
+        }
+        return RendererCapabilities.create(4, 16, 0);
+    }
+
+    @Override // com.google.android.exoplayer2.video.DecoderVideoRenderer
+    protected Decoder<DecoderInputBuffer, VideoDecoderOutputBuffer, FfmpegDecoderException> createDecoder(Format format, @Nullable CryptoConfig cryptoConfig) throws FfmpegDecoderException {
+        TraceUtil.beginSection("createFfmpegVideoDecoder");
+        int initialInputBufferSize = format.maxInputSize != -1 ? format.maxInputSize : DEFAULT_INPUT_BUFFER_SIZE;
+        Decoder decoder = new FfmpegVideoDecoder(this.numInputBuffers, this.numOutputBuffers, initialInputBufferSize, this.threads, format);
+        this.decoder = (FfmpegVideoDecoder) decoder;
+        TraceUtil.endSection();
+        Log.d(TAG, "createDecoder: " + decoder);
+        return decoder;
+    }
+
+    @Override // com.google.android.exoplayer2.video.DecoderVideoRenderer
+    protected void renderOutputBufferToSurface(VideoDecoderOutputBuffer outputBuffer, Surface surface) throws FfmpegDecoderException {
+        try {
+            if (this.decoder == null) {
+                throw new FfmpegDecoderException("Failed to render output buffer to surface: decoder is not initialized.");
+            }
+            this.decoder.renderToSurface(outputBuffer, surface);
+            outputBuffer.release();
+        } catch (Exception e) {
+        }
+    }
+
+    @Override // com.google.android.exoplayer2.video.DecoderVideoRenderer
+    protected void setDecoderOutputMode(int outputMode) {
+        if (this.decoder != null) {
+            this.decoder.setOutputMode(outputMode);
+        }
+    }
+
+    @Override // com.google.android.exoplayer2.Renderer, com.google.android.exoplayer2.RendererCapabilities
+    public String getName() {
+        return TAG;
+    }
+
+    @Override // com.google.android.exoplayer2.Renderer
+    public void setPlaybackSpeed(float currentPlaybackSpeed, float targetPlaybackSpeed) throws ExoPlaybackException {
+    }
+
+    @Override // com.google.android.exoplayer2.video.DecoderVideoRenderer
+    protected DecoderReuseEvaluation canReuseDecoder(String decoderName, Format oldFormat, Format newFormat) {
+        boolean sameMimeType = Util.areEqual(oldFormat.sampleMimeType, newFormat.sampleMimeType);
+        return new DecoderReuseEvaluation(decoderName, oldFormat, newFormat, sameMimeType ? 3 : 0, sameMimeType ? 0 : 8);
+    }
+}
